9adfb236e98b67de9b8627176deb7eb7ed8cb5b3
CenJim <94654311+CenJim@users.noreply.github.com>
2024-07-16 01:56:39 +0100

    evaluation

---
+++ evaluation.py
@@ -0,0 +1,66 @@
+import argparse
+from model.network import EHDR_network
+from PIL import Image
+import numpy as np
+import torch
+
+
+def data_load(group, device):
+    # 读取数据
+    ldr_image_1 = np.load(group[0])
+    ldr_image_2 = np.load(group[1])
+    ldr_image_3 = np.load(group[2])
+    events_1 = []
+    events_2 = []
+    with np.load(group[3]) as data:
+        for key in data:
+            events_1.append(data[key])
+    with np.load(group[4]) as data:
+        for key in data:
+            events_2.append(data[key])
+
+    # 转换输入数据为torch.Tensor
+    ldr_image_1_tensor = torch.from_numpy(ldr_image_1).float().unsqueeze(0).to(device)
+    ldr_image_2_tensor = torch.from_numpy(ldr_image_2).float().unsqueeze(0)
+    ldr_image_3_tensor = torch.from_numpy(ldr_image_3).float().unsqueeze(0)
+    events_1_tensor = torch.from_numpy(np.array(events_1)).float().unsqueeze(0)
+    events_2_tensor = torch.from_numpy(np.array(events_2)).float().unsqueeze(0)
+
+    return
+
+
+def main(model_name: str, pretrain_models: str, save_path: str):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    net = eval(model_name)(event_shape=(469, 640), num_feat=64, num_frame=3).to(device)
+    net.load(pretrain_models)
+    reference_image = '/home/s2491540/dataset/DSEC/train_sequences/sequence_0000000/ldr_images/000001_2.npy'
+    under_exposure = '/home/s2491540/dataset/DSEC/train_sequences/sequence_0000000/ldr_images/000000_1.npy'
+    over_exposure = '/home/s2491540/dataset/DSEC/train_sequences/sequence_0000000/ldr_images/000002_3.npy'
+    events_under = '/home/s2491540/dataset/DSEC/train_sequences/sequence_0000000/events/000000_1.npz'
+    events_upper = '/home/s2491540/dataset/DSEC/train_sequences/sequence_0000000/events/000001_2.npz'
+    group = (under_exposure, reference_image, over_exposure, events_under, events_upper)
+    input = data_load(group, device)
+    output = net(reference_image, under_exposure, over_exposure, events_under, events_upper)
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-network', type=str, default='EHDR_network')  # EVSNN_LIF_final or PAEVSNN_LIF_AMPLIF_final
+    # parser.add_argument('-path_to_pretrain_models', type=str, default='./pretrained_models/EVSNN.pth')  #
+    # parser.add_argument('-path_to_event_files', type=str, default='./data/poster_6dof_cut.txt')
+    # parser.add_argument('-save_path', type=str, default='./results')
+    # parser.add_argument('-height', type=int, default=180)
+    # parser.add_argument('-width', type=int, default=240)
+    # parser.add_argument('-num_events_per_pixel', type=float, default=0.5)
+    # args = parser.parse_args()
+    # model_name = args.network
+    # pretrain_models = args.path_to_pretrain_models
+    # event_files = args.path_to_event_files
+    # save_path = args.save_path
+    # height = args.height
+    # width = args.width
+    # num_events_per_pixel = args.num_events_per_pixel
+    model_name = 'EHDR_network'
+    pretrain_models = '/home/s2491540/Pythonproj/Multi-Bracket-HDR-Events/pretrained_models/EHDR.pth'
+    save_path = './result'
+    main(model_name, pretrain_models, save_path)
